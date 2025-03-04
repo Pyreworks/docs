4e6d17bc5548971f0cb2822a579394a3da71aa27
Shiawaseu <saud876360@gmail.com>
2025-03-04 22:49:43 +0300

    [FEAT] Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pyre-docs.vercel.app', // Switch to custom domain ?
   integrations: [
       starlight({
           title: 'Pyre Documentation',
